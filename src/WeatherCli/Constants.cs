900739faa28dacb4b8b6c869d04bb531fef3f844
Kristian Dimitrov <kdimitrov@develop-soft.com>
2020-11-30 21:47:11 +0200

    Update extended help text

--- src/WeatherCli/Constants.cs
+++ src/WeatherCli/Constants.cs
@@ -4,6 +4,8 @@
     {
         public const string ApiKeyEnvironmentVariable = "OPENWEATHERMAP_APIKEY";
 
-        public const string ExtendedHelpText = "You must provide your OpenWeatherMap API key to use this tool (see https://openweathermap.org/appid)";
+        public const string ExtendedHelpText =
+            "Get your API key at ( https://openweathermap.org/appid ) to use this tool. " +
+            "The API key must be provided either as an option to a command, or by setting the OPENWEATHERMAP_APIKEY environment variable.";
     }
 }
